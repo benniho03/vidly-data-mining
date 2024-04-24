8f7ca2be8427794be30ef4bc91383f4f30138e28
Holderle <wn00182469@wgs.wuerth.com>
2024-04-24 13:45:58 +0200

    added time measuring

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { getVideoIdsFromSearch } from "./youtube/search"
 import { getVideoDetails } from "./youtube/videos"
 
 
-const searchTerm = "minecraft"
-const maxResults = 250
+const searchTerm = "onlinemedien"
+const maxResults = 50
+
+const start = new Date().getTime()
 
 const videoIds = await getVideoIdsFromSearch({ searchTerm, maxResults })
 
@@ -11,4 +13,9 @@ console.log("Found", videoIds.length, "videos")
 
 const videoDetails = await getVideoDetails(videoIds)
 
+const end = new Date().getTime()
+const timeTaken = (end - start) / 1000
+
 Bun.write(`./data/${searchTerm}-videos.json`, JSON.stringify(videoDetails, null, 2))
+
+console.log((videoIds.length / timeTaken).toFixed(1), "videos per second")
