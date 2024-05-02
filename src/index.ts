68550a92b327b4d050bd35a480970617591fe819
on21OE <oli.ebert.21@lehre.mosbach.dhbw.de>
2024-05-02 14:55:48 +0200

    work in progress

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { getVideoIdsFromSearch } from "./youtube/search"
 import { getVideoDetails } from "./youtube/videos"
+import { PrismaClient } from '@prisma/client'
 
 
 const searchTerm = "theo t3 the modern react tutorial"
@@ -19,3 +20,40 @@ const timeTaken = (end - start) / 1000
 Bun.write(`./data/${searchTerm}-videos.json`, JSON.stringify(videoDetails, null, 2))
 
 console.log((videoIds.length / timeTaken).toFixed(1), "videos per second")
+
+const prisma = new PrismaClient()
+
+async function main() {
+    console.log("test")
+    const video = await prisma.video.create({
+        data: {
+            id: "1",
+            title: "",
+            thumbnail: "",
+            description: "",
+            channel: "",
+            likeCount: 0,
+            commentCount: 0,
+            viewCount: 0,
+            duration: "",
+            publishedAt: "",
+            caption: "",
+            tags: [""],
+            topicCategories: [""],
+            language: ""
+        },
+    })
+    console.log("test: ", video)
+    const getVideos = await prisma.video.findMany()
+    console.log("videos: ", getVideos)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
