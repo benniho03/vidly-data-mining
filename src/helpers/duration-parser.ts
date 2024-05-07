186e60b67e36e60aa153d7c437090f2bde115c7c
on21FU <flo.uhlschmiedmx.de>
2024-05-07 14:26:58 +0200

    duration parser is working

--- src/helpers/duration-parser.ts
+++ src/helpers/duration-parser.ts
@@ -1,32 +1,6 @@
+import { parse, toSeconds } from "iso8601-duration";
+
+
 export function iso8601ToSeconds(duration: string): number {
-    // Verwende ein reguläres Ausdrucksmuster, um die Teile der ISO-Dauer zu extrahieren
-    const matches = duration.match(/^P(?:([0-9]+)Y)?(?:([0-9]+)M)?(?:([0-9]+)W)?(?:([0-9]+)D)?(?:T(?:([0-9]+)H)?(?:([0-9]+)M)?(?:([0-9]+)S)?)?$/);
-    
-    if (!matches) {
-        throw new Error("Invalid ISO 8601 duration format");
-    }
-    
-    // Extrahiere die Teile der Dauer
-    const years = matches[1] ? parseInt(matches[1]) : 0;
-    const months = matches[2] ? parseInt(matches[2]) : 0;
-    const weeks = matches[3] ? parseInt(matches[3]) : 0;
-    const days = matches[4] ? parseInt(matches[4]) : 0;
-    const hours = matches[5] ? parseInt(matches[5]) : 0;
-    const minutes = matches[6] ? parseInt(matches[6]) : 0;
-    const seconds = matches[7] ? parseInt(matches[7]) : 0;
-
-    // Berechne die Gesamtzahl der Sekunden
-    const totalSeconds = (
-        years * 365 * 24 * 60 * 60 + // Annäherung an Jahre, ignoriert Schaltjahre
-        months * 30 * 24 * 60 * 60 + // Annäherung an Monate, ignoriert Schaltjahre
-        weeks * 7 * 24 * 60 * 60 + // Umrechnung von Wochen in Tage
-        days * 24 * 60 * 60 +
-        hours * 60 * 60 +
-        minutes * 60 +
-        seconds
-    );
-    // console.log("ISO Zeit: ", duration, " Sekunden Zeit: ", totalSeconds)
-
-    return totalSeconds;
+    return toSeconds(parse(duration));
 }
-
